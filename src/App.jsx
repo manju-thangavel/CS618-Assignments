4acf405f5e26a9d86f2a2b6aaa221bf188c71acd
Huseyin Ergin <mersinus@gmail.com>
2025-07-17 21:28:45 +0000

    eslint and prettier integrated

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,20 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank' rel='noreferrer'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Hello CS618</h1>
-      <div className="card">
+      <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          
-        </p>
+        <p></p>
       </div>
-
     </>
   )
 }
