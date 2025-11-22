6b873db6901d4913187e173953844711613093bd
manju-thangavel <srimanjunath.thangavel@hotmail.com>
2025-11-22 19:10:30 +0000

    chore: committing till containers

--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -7,7 +7,7 @@ import {
   updatePost,
   createPost,
 } from '../services/posts.js'
-
+import { requireAuth } from '../middleware/jwt.js'
 export function postsRoutes(app) {
   app.get('/api/v1/posts', async (req, res) => {
     const { sortBy, sortOrder, author, tag } = req.query
@@ -40,7 +40,7 @@ export function postsRoutes(app) {
       return res.status(500).end()
     }
   })
-  app.post('/api/v1/posts', async (req, res) => {
+  app.post('/api/v1/posts', requireAuth, async (req, res) => {
     try {
       const post = await createPost(req.body)
       return res.json(post)
@@ -49,7 +49,7 @@ export function postsRoutes(app) {
       return res.status(500).end()
     }
   })
-  app.patch('/api/v1/posts/:id', async (req, res) => {
+  app.patch('/api/v1/posts/:id', requireAuth, async (req, res) => {
     try {
       const post = await updatePost(req.params.id, req.body)
       return res.json(post)
@@ -58,7 +58,7 @@ export function postsRoutes(app) {
       return res.status(500).end()
     }
   })
-  app.delete('/api/v1/posts/:id', async (req, res) => {
+  app.delete('/api/v1/posts/:id', requireAuth, async (req, res) => {
     try {
       const { deletedCount } = await deletePost(req.params.id)
       if (deletedCount === 0) return res.sendStatus(404)
